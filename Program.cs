750f1e60b5411316c74d677f32fbfb24dec42148
Alexy102 <59171672+Alexy102@users.noreply.github.com>
2019-12-23 20:53:14 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double symm = 0;
+            Console.WriteLine("Введите число n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i<=n;i++)
+            {
+                symm = symm + (1 + (1 / Math.Pow(i, 2)));
+            }
+            Console.WriteLine("Сумма равна = " + symm);
+        }
+    }
+}
